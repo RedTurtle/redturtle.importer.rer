daeab3750a47d7d5cb2b97f8853bacc3c16a32ac
Filippo Campi <filippo.campi@redturtle.it>
2018-07-11 11:50:24 +0200

    [dev] added procedure to fix multipler video link domain

--- src/redturtle/importer/rer/browser/migrations.py
+++ src/redturtle/importer/rer/browser/migrations.py
@@ -8,10 +8,30 @@ from redturtle.importer.base import logger
 from redturtle.importer.base.browser.migrations import RedTurtlePlone5MigrationMain  # noqa
 from rer.linknormativa.base.interfaces import INormativaType
 from transmogrify.dexterity.interfaces import IDeserializer
+from urlparse import urlparse
 from zope.component import getAdapter
 from zope.event import notify
 from zope.lifecycleevent import ObjectModifiedEvent
 from zope.schema import getFieldsInOrder
+
+import transaction
+
+
+def _domain_extractor(url):
+    name = urlparse(url)[1].replace('www.', '')
+    # print u"Dominio rilevato: {}".format(name)
+    return name
+
+
+def _domain_fix(url):
+    """ Questa funzione prende un url con dominio che finisce per '.tv' e lo
+    sistema mettendoci .it per poi restituirlo.
+    """
+    fixed_url = url.replace(
+        'multipler.lepida.tv',
+        'multipler.lepida.it',
+    )
+    return fixed_url
 
 
 class RERPlone5MigrationMain(RedTurtlePlone5MigrationMain):
@@ -63,6 +83,7 @@ class RERPlone5MigrationMain(RedTurtlePlone5MigrationMain):
 
     def scripts_post_migration(self):
         self.fix_linkNormativa()
+        self.fix_multipler_video_link()
 
     def fix_linkNormativa(self):
         brains = api.content.find(portal_type='LinkNormativa')
@@ -82,3 +103,51 @@ class RERPlone5MigrationMain(RedTurtlePlone5MigrationMain):
             setattr(normativa, 'remoteUrl', normativa_url)
 
             logger.warn('Fixed {0}'.format(normativa.absolute_url()))
+
+    def fix_multipler_video_link(self):
+        urls_changed = 0  # used for partial commit
+        total_urls_changes = 0
+        empty_urls = 0
+
+        site = api.portal.get()
+        print u'Getting all the video objects in the site...'
+        video_brains = site.portal_catalog.unrestrictedSearchResults(
+            portal_type=['WildcardVideo'],
+        )
+        print u'Checkin all the URLs...'
+        for brain in video_brains:
+            video_obj = brain.getObject()
+            url = video_obj.video_url
+            if url:
+                domain = _domain_extractor(url)
+                if domain.endswith('.tv'):
+                    urls_changed += 1
+                    print u'Fixing url for: {0}'.format(brain.getPath())
+                    fixed_url = _domain_fix(url)
+
+                    print u'From:\t{0}\nTo:\t{1}'.format(
+                        url,
+                        fixed_url,
+                    )
+
+                    video_obj.video_url = fixed_url
+                    total_urls_changes += 1
+
+                if urls_changed > 10:
+                    try:
+                        print 'Partial Commit...'
+                        transaction.commit()
+                        print 'Partial Commit: OK'
+                    except Exception as e:
+                        logger.error(
+                            u'Error while committing transaction.')
+                        logger.error(u'{0}'.format(e))
+                    urls_changed = 0
+            else:
+                empty_urls += 1
+                print u'ERROR: Wrong video_url field (empty) for: {0}'.format(
+                    brain.getPath())
+
+        print u'Total Video objects: {0}'.format(len(video_brains))
+        print u'Video with empty URL fields: {0}'.format(empty_urls)
+        print u'Total Video URLs changes: {0}'.format(total_urls_changes)
